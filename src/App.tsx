12fb59879ae9ca43d0b8b6bdefcc7a8026cd3dd4
ViacheslavPiskun <pterophyllum.dev@gmail.com>
2023-08-22 00:32:51 +0300

    add router and code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import Counter from "./components/Counter";
+import { Suspense } from "react";
+import { Routes, Route, Link } from 'react-router-dom'
+
+import { AboutPageAsync } from "./components/pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./components/pages/MainPage/MainPage.async";
 
 import './index.scss'
 
 const App = () => {
     return (
         <div className="app">
-            <Counter />
-            <Counter />
+            <header>
+                <Link to={'/'}>main</Link>
+                <Link to={'/about'}>about</Link>
+            </header>
+            <Suspense fallback={'<div>loading...</div>'}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
